fce1207514b54bf5e28166be08e62a90e65481d5
Devin Douglas <djdouglas01@gmail.com>
2023-07-09 13:02:17 +0100

    Changed styling and added JS, create rolling page

---
+++ Assests/index.js
@@ -0,0 +1,17 @@
+function scrollToSection(sectionId) {
+    const section = document.querySelector(sectionId);
+    window.scrollTo({
+      top: section.offsetTop,
+      behavior: 'smooth'
+    });
+  }
+
+  // Attach click event listeners to navbar links
+  const navLinks = document.querySelectorAll('.nav-link');
+  navLinks.forEach(link => {
+    link.addEventListener('click', event => {
+      event.preventDefault(); // Prevent default anchor click behavior
+      const targetSection = event.target.getAttribute('href');
+      scrollToSection(targetSection);
+    });
+  });
